91f4a06e0fb334d4367e9a671b5844af77cf6b5b
Upshur06 <mupshur2005@gmail.com>
2021-11-11 02:46:26 -0500

    top banner design

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from '../axios'
 import requests from '../request';
+import '../css/Banner.css';
 
 export const Banner = () => {
     const [movie, setMovie] = useState([]);
@@ -20,9 +21,38 @@ export const Banner = () => {
     }, [])
 
         console.log(movie);
+
+        function truncate(str, n){
+            return str?.length > n ? str.substr(0, n-1) + '...' : str;
+        }
+
     return (
-        <header>
-            
+        <header className='banner'
+            style={{
+                background: 'contain',
+                backgroundImage: `url(
+                    'http://image.tmdb.org/t/p/original/${movie?.backdrop_path}'
+                )`,
+                backgroundPosition: 'center center',
+
+            }}
+        >
+            <div className="banner_contents">
+                <h1 className='banner_title'>
+                    { movie?.title || movie?.name || movie?.original_name }
+                </h1>
+
+                <div className="banner_buttons">
+                    <button className="banner_button">Play</button>
+                    <button className="banner_button">My List</button>
+                </div>
+
+                <h1 className="banner_description">
+                    { truncate(movie?.overview, 150) }
+                </h1>
+            </div>
+
+            <div className="banner_fadeBottom"></div>
         </header>
     )
 }
